688f8f862c24fc4d6ad73d59ffd2e2cb748f962e
Victo0ri <113626801+Victo0ri@users.noreply.github.com>
2025-09-11 10:42:01 +0200

    Add thread for blink lamp

--- alarm.lua
+++ alarm.lua
@@ -3,7 +3,12 @@ local sides = require("sides")
 local colors = require("colors")
 local term = require("term")
 local keyboard = require("keyboard")
+local event = require("event")
+local thread = require("thread")
 local rs = component.redstone
+
+thread.create(mainLoop)
+thread.create(blinkLoop)
 
 local function Exit() -- Exit the program
     term.clear()
@@ -23,47 +28,60 @@ local function SetSmall(sides, value) -- Activate the alarm (Warning)
     rs.setOutput(sides, value)
 end
 
-while true do
-------------------------Set the colors-------------------------------------------------------------
-local white = inputB(sides.south, colors.white)
-local black = inputB(sides.south, colors.black)
-local lime = inputB(sides.south, colors.lime)
-local yellow = inputB(sides.south, colors.yellow)
----------------------------------------------------------------------------------------------------
 
-    term.clear()
-    
-    if white > 1 then -- Bundel from Cleanroom
-        print("\n/!\\ Emergency /!\\\nAlarme from Cleanroom !")
+local function blinkLoop()
+    while true do
+        rs.setOutput(sides.top, 15)
+        event.pull(0,5)
+        rs.setOutput(sides.top, 0)
     end
-
-    if lime > 1 then -- Bundel from intensive process
-        print("\n/!\\ Emergency /!\\\nAlarme from intensive production !")
-    end
-
-    if black > 1 then -- Bundel from Benzene production
-        print("\n! Warning !\nAlarme from Benzene production !")
-    end
-
-    if yellow > 1 then -- Bundel from passive process
-        print("\n! Warning !\nAlarme from passive production !")
-    end
-
-    if white > 1 or lime > 1 then -- Cleanroom, intensive process alarm
-        SetBig(sides.north, 15)
-    elseif black > 1 or yellow > 1 then -- Benzene, passive process alarm
-        SetSmall(sides.east, 15)
-    else
-        SetBig(sides.north, 0)
-        SetSmall(sides.east, 0)
-    end
-
-    os.sleep(2)
-
-    if keyboard.isShiftDown() and keyboard.isControlDown() then
-        break
-    end
-
 end
+
+
+local function mainLoop()
+    while true do
+    ------------------------Set the colors-------------------------------------------------------------
+    local white = inputB(sides.south, colors.white)
+    local black = inputB(sides.south, colors.black)
+    local lime = inputB(sides.south, colors.lime)
+    local yellow = inputB(sides.south, colors.yellow)
+    ---------------------------------------------------------------------------------------------------
+    
+        term.clear()
+        
+        if white > 1 then -- Bundel from Cleanroom
+            print("\n/!\\ Emergency /!\\\nAlarme from Cleanroom !")
+        end
+    
+        if lime > 1 then -- Bundel from intensive process
+            print("\n/!\\ Emergency /!\\\nAlarme from intensive production !")
+        end
+    
+        if black > 1 then -- Bundel from Benzene production
+            print("\n! Warning !\nAlarme from Benzene production !")
+        end
+    
+        if yellow > 1 then -- Bundel from passive process
+            print("\n! Warning !\nAlarme from passive production !")
+        end
+    
+        if white > 1 or lime > 1 then -- Cleanroom, intensive process alarm
+            SetBig(sides.north, 15)
+        elseif black > 1 or yellow > 1 then -- Benzene, passive process alarm
+            SetSmall(sides.east, 15)
+        else
+            SetBig(sides.north, 0)
+            SetSmall(sides.east, 0)
+        end
+    
+        os.sleep(2)
+    
+        if keyboard.isShiftDown() and keyboard.isControlDown() then
+            break
+        end
+    end
+end
+
+waitForAll()
 
 Exit()
