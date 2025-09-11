8b9534aeb7f1134b2fbeeb644232a95c368dccaa
Victo0ri <113626801+Victo0ri@users.noreply.github.com>
2025-09-11 20:01:29 +0200

    V1 : blink lamp, set the alarm

--- alarm.lua
+++ alarm.lua
@@ -4,16 +4,34 @@ local colors = require("colors")
 local term = require("term")
 local keyboard = require("keyboard")
 local event = require("event")
-local thread = require("thread")
 local rs = component.redstone
-
-thread.create(mainLoop)
-thread.create(blinkLoop)
 
 local function Exit() -- Exit the program
     term.clear()
     os.exit()
 end
+
+------------------------Clignottement lamp---------------------------------------------------------
+local blinkTimer = nil
+local state = false
+
+local function startBlink()
+    if not blinkTimer then
+        blinkTimer = event.timer(0.5, function()
+            state = not state
+            rs.setOutput(sides.top, state and 15 or 0)
+        end, math.huge)
+    end
+end
+
+local function stopBlink()
+    if blinkTimer then
+        event.cancel(blinkTimer)
+        blinkTimer = nil
+        rs.setOutput(sides.top, 0)
+    end
+end
+---------------------------------------------------------------------------------------------------
 
 local function inputB(sides, colors) -- Get the input from the bundle
     local result = rs.getBundledInput(sides, colors)
@@ -22,66 +40,59 @@ end
 
 local function SetBig(sides, value) -- Activate the alarm (Emergency)
     rs.setOutput(sides, value)
+    startBlink()
 end
 
 local function SetSmall(sides, value) -- Activate the alarm (Warning)
     rs.setOutput(sides, value)
 end
 
-
-local function blinkLoop()
-    while true do
-        rs.setOutput(sides.top, 15)
-        event.pull(0,5)
-        rs.setOutput(sides.top, 0)
-    end
-end
-
-
-local function mainLoop()
-    while true do
-    ------------------------Set the colors-------------------------------------------------------------
+while true do
+    ------------------------Set the colors-----------------------------------------------------
     local white = inputB(sides.south, colors.white)
     local black = inputB(sides.south, colors.black)
     local lime = inputB(sides.south, colors.lime)
     local yellow = inputB(sides.south, colors.yellow)
-    ---------------------------------------------------------------------------------------------------
+    -------------------------------------------------------------------------------------------
+
+    term.clear()
     
-        term.clear()
-        
-        if white > 1 then -- Bundel from Cleanroom
-            print("\n/!\\ Emergency /!\\\nAlarme from Cleanroom !")
-        end
-    
-        if lime > 1 then -- Bundel from intensive process
-            print("\n/!\\ Emergency /!\\\nAlarme from intensive production !")
-        end
-    
-        if black > 1 then -- Bundel from Benzene production
-            print("\n! Warning !\nAlarme from Benzene production !")
-        end
-    
-        if yellow > 1 then -- Bundel from passive process
-            print("\n! Warning !\nAlarme from passive production !")
-        end
-    
-        if white > 1 or lime > 1 then -- Cleanroom, intensive process alarm
-            SetBig(sides.north, 15)
-        elseif black > 1 or yellow > 1 then -- Benzene, passive process alarm
-            SetSmall(sides.east, 15)
-        else
-            SetBig(sides.north, 0)
-            SetSmall(sides.east, 0)
-        end
-    
-        os.sleep(2)
-    
-        if keyboard.isShiftDown() and keyboard.isControlDown() then
-            break
-        end
+    if white > 1 then -- Bundel from Cleanroom
+        print("\n/!\\ Emergency /!\\\nAlarme from Cleanroom !")
+    end
+
+    if lime > 1 then -- Bundel from intensive process
+        print("\n/!\\ Emergency /!\\\nAlarme from intensive production !")
+    end
+
+    if black > 1 then -- Bundel from Benzene production
+        print("\n! Warning !\nAlarme from Benzene production !")
+    end
+
+    if yellow > 1 then -- Bundel from passive process
+        print("\n! Warning !\nAlarme from passive production !")
+    end
+
+    if white > 1 or lime > 1 then -- Turn ON the big alarm
+        SetBig(sides.north, 15)
+        SetSmall(sides.east, 0)
+    elseif black > 1 or yellow > 1 then -- Turn ON the small alarm
+        stopBlink()
+        SetSmall(sides.east, 15)
+        rs.setOutput(sides.top, 15)
+        rs.setOutput(sides.north, 0)
+    else -- Turn OFF all alarm
+        SetBig(sides.north, 0)
+        SetSmall(sides.east, 0)
+        rs.setOutput(sides.top, 0)
+        stopBlink()
+    end
+
+    os.sleep(2)
+
+    if keyboard.isShiftDown() and keyboard.isControlDown() then
+        break
     end
 end
-
-waitForAll()
 
 Exit()
